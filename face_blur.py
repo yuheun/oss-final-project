7cf93a8ce5f7b3a89bb78d1135be19c7cd240bc7
SeungTaek Oh <austin000101@gachon.ac.kr>
2024-12-12 03:11:42 +0900

    Second Update face_blur.py

--- face_blur.py
+++ face_blur.py
@@ -20,3 +20,22 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+for (x, y, w, h) in faces:
+        # face detection
+        face_region = frame[y:y+h, x:x+w]
+        
+        # Gaussian Blur
+        blurred_face = cv2.GaussianBlur(face_region, (99, 99), 30)
+        
+        frame[y:y+h, x:x+w] = blurred_face
+
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    cv2.imshow('Face Blur', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
